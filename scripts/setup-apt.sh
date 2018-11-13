1464911daff9eecc2e838f221d74ea7b31bb850f
phineas_WM <phineas@wm.com>
2018-11-13 11:22:00 +0100

    added autodetection for package manager based on distro name
    This allow the installation scripts to use the correct package manager
    in order to work on centos, fedora, ubuntu, debian and archlinux.

--- scripts/setup-apt.sh
+++ scripts/setup-apt.sh
@@ -1,7 +1,58 @@
+function command-not-found {
+    echo "[ERROR] command \"$1\" not found on your system..."
+    exit 1
+}
+
+function get-distro {
+    distro=$(cat /etc/*release | grep '^ID=' | cut -d '=' -f 2)
+    cmd=""
+    case "$distro" in
+	"arch")
+	    which pacman
+	    if [ $? == 0 ]; then
+		cmd="pacman -S --noconfirm "
+	    else
+		command-not-found pacman
+	    fi
+	    ;;
+	"debian" | "ubuntu")
+	    which apt-get
+	    if [ $? == 0 ]; then
+		cmd="apt-get install -y "
+	    else
+		command-not-found apt-get
+	    fi
+	    ;;
+	"centos")
+	    which yum
+	    if [ $? == 0 ]; then
+		cmd="yum install -y "
+	    else
+		command-not-found yum
+	    fi
+	    ;;
+	"fedora")
+	    which dnf
+	    if [ $? == 0 ]; then
+		cmd="dnf install -y "
+	    else
+		command-not-found dnf
+	    fi
+	    ;;
+	*)
+	    echo "[ERROR] unsupported distribution... exiting"
+	    exit 1
+	    ;;
+
+	echo "[INFO] detected linux distribution: \"$distro\", using \"$cmd\" for package installation"
+    esac
+}
+
 function apt-install {
+    get-distro
     for pkg in $@; do
-        echo -e "[APT-GET] Installing package $pkg..."
-        sudo apt-get install -yq $pkg
+        echo -e "[INFO] Installing package $pkg..."
+        sudo $cmd $pkg
     done
 }
 
@@ -16,9 +67,95 @@ function apt-install-if-needed {
 
 
 function package-not-installed {
-    test -z "$(sudo dpkg -s $1 2> /dev/null | grep Status)"
+    distro=$(cat /etc/*release | grep '^ID=' | cut -d '=' -f 2)
+    cmd=""
+    case "$distro" in
+	"arch")
+	    which pacman
+	    if [ $? == 0 ]; then
+		sudo pacman -Qi $1
+	    else
+		command-not-found pacman
+	    fi
+	    ;;
+	"debian" | "ubuntu")
+	    which apt-get
+	    if [ $? == 0 ]; then
+		sudo dpkg -s $1 2> /dev/null | grep Status
+	    else
+		command-not-found apt-get
+	    fi
+	    ;;
+	"centos")
+	    which yum
+	    if [ $? == 0 ]; then
+		sudo yum list installed $1
+	    else
+		command-not-found yum
+	    fi
+	    ;;
+	"fedora")
+	    which dnf
+	    if [ $? == 0 ]; then
+		sudo dnf list installed $1
+	    else
+		command-not-found dnf
+	    fi
+	    ;;
+	*)
+	    echo "[ERROR] unsupported distribution... exiting"
+	    exit 1
+	    ;;
+
+	echo "[INFO] detected linux distribution: \"$distro\", using \"$cmd\" for packages updates"
+	$cmd
+    esac    
 }
 
-sudo apt-get -y update
-sudo apt-get -y upgrade
-sudo apt-get -y dist-upgrade
+function update-packages {
+    distro=$(cat /etc/*release | grep '^ID=' | cut -d '=' -f 2)
+    cmd=""
+    case "$distro" in
+	"arch")
+	    which pacman
+	    if [ $? == 0 ]; then
+		cmd="pacman -Syyu --noconfirm"
+	    else
+		command-not-found pacman
+	    fi
+	    ;;
+	"debian" | "ubuntu")
+	    which apt-get
+	    if [ $? == 0 ]; then
+		cmd="apt-get update -y; apt-get upgrade -y"
+	    else
+		command-not-found apt-get
+	    fi
+	    ;;
+	"centos")
+	    which yum
+	    if [ $? == 0 ]; then
+		cmd="yum update -y "
+	    else
+		command-not-found yum
+	    fi
+	    ;;
+	"fedora")
+	    which dnf
+	    if [ $? == 0 ]; then
+		cmd="dnf update -y "
+	    else
+		command-not-found dnf
+	    fi
+	    ;;
+	*)
+	    echo "[ERROR] unsupported distribution... exiting"
+	    exit 1
+	    ;;
+
+	echo "[INFO] detected linux distribution: \"$distro\", using \"$cmd\" for packages updates"
+	$cmd
+    esac
+}
+
+sudo update-packages
